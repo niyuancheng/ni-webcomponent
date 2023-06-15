a33be70cb1c952d41f31bd8ab49fb1ca8925bca7
niyuancheng <nyc20010820@126.com>
2023-06-15 23:02:27 +0800

    fix: 完善组件的基础设置

--- packages/ui/babel.config.js
+++ packages/ui/babel.config.js
@@ -1,7 +1,7 @@
 //TODO 该文件为babel的配置文件
 const presets = [["@babel/preset-env"], ["@babel/preset-typescript"]]
 const plugins = [
-    //TODO 此处为重点！！！需要注意的是插件@babel/plugin-transform-runtime要和@babel/runtime搭配使用，才能对ECMAScript的新api进行编译和polyfill
+    //TODO 此处为重点！！！需要注意的是插件@babel/plugin-transform-runtime要和 搭配使用，才能对ECMAScript的新api进行编译和polyfill
     ["@babel/plugin-transform-runtime"],
     [
         "@babel/plugin-proposal-decorators",
